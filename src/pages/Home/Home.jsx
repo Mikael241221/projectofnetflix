281ceffd7615c5f8552dd960f49548e74133af21
unknown <bashawdejenu1221@gmail.com>
2024-12-28 23:47:24 +0300

    create a banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../../componets/Header/Header'
 import Footer from '../../componets/Footer/Footer'
+import Banner from '../../componets/Banner/Banner'
 
 
 const Home = () => {
   return (
     <>
       <Header/>
+      <Banner/>
       <Footer/>
     </>
   )
