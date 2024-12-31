d32b29572a99ec0b19e052e00d547c42b4d7f1e6
unknown <bashawdejenu1221@gmail.com>
2024-12-31 21:41:15 +0300

    setup movietrailer and react-youtube

--- src/componets/Rows/RowList/RowList.jsx
+++ src/componets/Rows/RowList/RowList.jsx
@@ -9,12 +9,42 @@ const RowList = () => {
       <Row 
       title= "NETFLIX ORIGINALS"
       fetchUrl={requests.fetchNetflixOriginals}
-      isLargeRow ={false}
+      isLargeRow ={true}
       />
-      {/* <Row/>
-      <Row/>
-      <Row/>
-      <Row/> */}
+      <Row 
+      title= "Treding Now"
+      fetchUrl={requests.fetchTrending}
+      
+      />
+      <Row 
+      title= "Top Rated "
+      fetchUrl={requests.fetchTopRatedMovies}
+      />
+      <Row 
+      title= "Action Movies"
+      fetchUrl={requests.fetchActionMovies}
+      />
+      <Row 
+      title= "Comedy Movies"
+      fetchUrl={requests.fetchComedyMovies}
+      />
+      <Row 
+      title= "Horror Movies"
+      fetchUrl={requests.fetchHorrorMovies}
+      />
+      <Row 
+      title= "Romance Movies"
+      fetchUrl={requests.fetchRomanceMovies}
+      />
+      <Row 
+      title= "TV Show"
+      fetchUrl={requests.fetchTvShow}
+      />
+      <Row 
+      title= "Documentaries"
+      fetchUrl={requests.fetchDocumentaries}
+      />
+      
     </>
   )
 }
